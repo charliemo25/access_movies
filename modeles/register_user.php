c9f793250dd3fa4dd47c6f7651fd9cced4e82484
unknown <moreaucharlie25@gmail.com>
2017-11-29 14:54:20 +0100

    hash256

--- modeles/register_user.php
+++ modeles/register_user.php
@@ -8,12 +8,29 @@ if(empty($_REQUEST['nom']) && empty($_REQUEST['prenom']) && empty($_REQUEST['pse
     
 } else {
     
+    $envoi = true;
     
-    $mdp = $_REQUEST['mdp'];
+    $nom = $_REQUEST['nom'];
+    $prenom = $_REQUEST['prenom'];
+    $pseudo = $_REQUEST['pseudo'];
+    $mdp = hash('sha256', $_REQUEST['mdp']);
+    
+    if($nom == '' || $prenom == '' || $pseudo == '' || $mdp == ''){
+        
+        echo'<p>Veuillez remplir les champs';
+        $envoi = false;
+        
+    }
     
     if(strlen($mdp) >10 || strlen($mdp)<8){
-        echo "<p>Le mot de passe doit être compris entre 8 et 12 caractères</p>";  
-    } else {
+        
+        echo "<p>Le mot de passe doit être compris entre 8 et 12 caractères</p>";
+        $envoi = false;
+        
+    }
+    
+    if($envoi == true){
+        
         $req = $pdo->prepare("insert into utilisateurs(nom, prenom, pseudo, mdp) values(:nom, :prenom, :pseudo, :mdp)");
         $req->bindParam(':nom', $_REQUEST['nom']);
         $req->bindParam(':prenom', $_REQUEST['prenom']);
