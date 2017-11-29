c4cf7342b88df043262f7eaa7057e50cbab5ec94
unknown <moreaucharlie25@gmail.com>
2017-11-29 13:50:13 +0100

    ajout d'utilisateur

--- modeles/register_user.php
+++ modeles/register_user.php
@@ -1,6 +1,23 @@
 <?php 
 
+var_dump($_REQUEST);
 
+if(empty($_REQUEST['nom']) && empty($_REQUEST['prenom']) && empty($_REQUEST['pseudo']) && empty($_REQUEST['mdp'])){
+    
+    echo "<p>Rempli ça sérieusement jeune bipède ! </p>";
+    
+} else {
+    
+    $mdp = password_hash($_REQUEST['mdp'], PASSWORD_DEFAULT);
+    
+    $req = $pdo->prepare("insert into utilisateurs(nom, prenom, pseudo, mdp) values(:nom, :prenom, :pseudo, :mdp)");
+    $req->bindParam(':nom', $_REQUEST['nom']);
+    $req->bindParam(':prenom', $_REQUEST['prenom']);
+    $req->bindParam(':pseudo', $_REQUEST['pseudo']);
+    $req->bindParam(':mdp', $mdp);
+    $req->execute();
+    
+}
 
 
 
