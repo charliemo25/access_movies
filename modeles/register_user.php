51ec7e4f23cd063af5f7434ea9248234adf600cd
unknown <moreaucharlie25@gmail.com>
2017-11-29 14:14:31 +0100

    condition sur le mdp

--- modeles/register_user.php
+++ modeles/register_user.php
@@ -8,14 +8,21 @@ if(empty($_REQUEST['nom']) && empty($_REQUEST['prenom']) && empty($_REQUEST['pse
     
 } else {
     
-    $mdp = password_hash($_REQUEST['mdp'], PASSWORD_DEFAULT);
     
-    $req = $pdo->prepare("insert into utilisateurs(nom, prenom, pseudo, mdp) values(:nom, :prenom, :pseudo, :mdp)");
-    $req->bindParam(':nom', $_REQUEST['nom']);
-    $req->bindParam(':prenom', $_REQUEST['prenom']);
-    $req->bindParam(':pseudo', $_REQUEST['pseudo']);
-    $req->bindParam(':mdp', $mdp);
-    $req->execute();
+    $mdp = $_REQUEST['mdp'];
+    
+    if(strlen($mdp) >10 || strlen($mdp)<8){
+        echo "<p>Le mot de passe doit être compris entre 8 et 12 caractères</p>";  
+    } else {
+        $req = $pdo->prepare("insert into utilisateurs(nom, prenom, pseudo, mdp) values(:nom, :prenom, :pseudo, :mdp)");
+        $req->bindParam(':nom', $_REQUEST['nom']);
+        $req->bindParam(':prenom', $_REQUEST['prenom']);
+        $req->bindParam(':pseudo', $_REQUEST['pseudo']);
+        $req->bindParam(':mdp', $mdp);
+        $req->execute();
+    }
+    
+    
     
 }
 
